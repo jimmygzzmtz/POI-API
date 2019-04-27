6047d26e72171b80a6d61e42d1b5a7c7216e562a
Jaime González <jimmygzzmtz@gmail.com>
2019-04-26 23:30:07 -0500

    Preparation for Heroku
    Changed username and password for MongoDB to Environment Variables.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 
-const connectionURL = 'mongodb+srv://jimmygzzmtz:<password>@cluster0-ae70y.mongodb.net/poiapi?retryWrites=true'
+var MongoUser = process.env.MongoUser
+var MongoPassword = process.env.MongoPassword
+
+const connectionURL = 'mongodb+srv://' + MongoUser + ':' + MongoPassword + '@cluster0-ae70y.mongodb.net/poiapi?retryWrites=true'
 
 mongoose.connect( connectionURL, {
   useNewUrlParser: true,
