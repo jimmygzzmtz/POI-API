8ca20fc9cec5f3eb5db7c73913dbc32b04d94414
Jaime González <jimmygzzmtz@gmail.com>
2019-04-27 03:17:21 -0500

    CORS Fix Attempt

--- src/app.js
+++ src/app.js
@@ -21,6 +21,12 @@ app.use(express.json())
 
 //Admin
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/:username/:password/pois', function(req, res){
   
   Account.find({ username: req.params.username, password: req.params.password}).then(function(account) {
