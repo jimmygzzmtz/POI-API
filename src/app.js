a56b150519ec3e438253d43833ef7e6cf145d3bc
Jaime González <jimmygzzmtz@gmail.com>
2019-04-27 21:17:42 -0500

    .

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.json())
 //Admin
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
