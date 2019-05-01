8a48ccb58a6d972d1438b3a8af03def3373bf9b4
Jaime González <jimmygzzmtz@gmail.com>
2019-05-01 02:57:19 -0500

    Cors Fix Attempt 2

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+express.use(cors())
+
+express.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+ });
+
 const controller = require('./controllers/users.js')
 // const auth = require('./middleware/auth')
 
